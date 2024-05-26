fb21f64c89c7336a7bde005f85f472b8f88d72ba
Guilherme Zanin <guilhermecasazanin@gmail.com>
2024-05-26 17:58:26 -0300

    novos estudos

--- explorando_leitura_arquivos/ler_arquivo/src/Main.java
+++ explorando_leitura_arquivos/ler_arquivo/src/Main.java
@@ -1,17 +1,21 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("src/resource/mnt/file.txt");
+
+//        MODELO #1
+
+        File file = new File("src/resource/mnt/file");
         Scanner sc = null;
 
+        System.out.println("\n");
+        
         try{
             sc = new Scanner(file);
             while (sc.hasNextLine()){
                 String line = sc.nextLine();
-                System.out.println(line);
+                System.out.println("METODO #1: " + line);
             }
 
         }catch (IOException e){
@@ -20,6 +24,47 @@ public class Main {
             if (sc != null){
                 sc.close();
             }
+        }
+
+//        MODELO #2
+        String path = "src/resource/mnt/file";
+        FileReader fr = null;
+        // gerencia a memoria para leitura
+        BufferedReader br = null;
+
+        System.out.println("\n");
+
+        try{
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+            String line = br.readLine();
+
+            while (line != null){
+                System.out.println("METODO #2: " + line);
+                line = br.readLine();
+            }
+        }catch (IOException e){
+            System.out.println("Erro: " + e.getMessage());
+        }finally {
+            try{
+                br.close();
+                fr.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+//        METODO #3 -> melhoria do #2 (try with resources)
+        try(BufferedReader br2 = new BufferedReader(new FileReader(path))){
+            String line = br2.readLine();
+
+            System.out.println("\n");
+            while (line != null){
+                System.out.println("METODO #3: " + line);
+                line = br2.readLine();
+            }
+        }catch (IOException e){
+            System.out.println("Erro: " + e.getMessage());
         }
 
     }
