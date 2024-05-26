1aa028a552a952fdecdd88f0395229ded002eed5
Guilherme Zanin <guilhermecasazanin@gmail.com>
2024-05-26 17:42:06 -0300

    novos estudos

--- explorando_leitura_arquivos/ler_arquivo/src/Main.java
+++ explorando_leitura_arquivos/ler_arquivo/src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File file = new File("src/resource/mnt/file.txt");
+        Scanner sc = null;
+
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                String line = sc.nextLine();
+                System.out.println(line);
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if (sc != null){
+                sc.close();
+            }
+        }
+
     }
 }
